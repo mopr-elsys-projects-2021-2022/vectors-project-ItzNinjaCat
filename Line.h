9fb47676e4a15fa7b6c86c8bd3f9854f18c07c01
kotka <47333381+ItzNinjaCat@users.noreply.github.com>
2021-12-10 01:46:06 +0200

    Update Line.h

--- Line.h
+++ Line.h
@@ -1,6 +1,8 @@
 #pragma once
+#include "Point.h"
 
-class Line {
-	double A, B, C;
+struct Line {
+	double A, B;
+	Line(double A = 0, double B = 0) : A(A), B(B) {}
+	Line(Point p1, Point p2) : A((p1.y - p2.y) / (p1.x - p2.x)), B(p1.y - ((p1.y - p2.y) / (p1.x - p2.x)) * p1.x) {}
 };
-
