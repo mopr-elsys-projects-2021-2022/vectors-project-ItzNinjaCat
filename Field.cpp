df63338dac8f82a04920bf566272572f4a96fca6
kotka <47333381+ItzNinjaCat@users.noreply.github.com>
2021-12-10 02:31:52 +0200

    Add files via upload

--- Field.cpp
+++ Field.cpp
@@ -19,20 +19,40 @@ Field::Field(Point endPoints[4], Ball ball) {
 	this->ball = ball;
 }
 
-void Field::hit(Point target, double power) {
+int Field::collision(Line ball_l, Line walls[4], Point cmp_p){
+	Point perp_point_tmp;
+	int bounce_index = -1;
+	for(int i = 0; i < 4; i++){
+		perp_point_tmp = Point((ball_l.B - walls[i].B) / (walls[i].A - ball_l.A), (ball_l.A * (ball_l.B - walls[i].B) / (walls[i].A - ball_l.A)) + ball_l.B);
+		if((cmp_p.x < perp_point_tmp.x and perp_point_tmp.x < ball.center.x) and (cmp_p.y < perp_point_tmp.y and perp_point_tmp.y < ball.center.y)){
+			bounce_index = i;
+		}
+		else if((cmp_p.x > perp_point_tmp.x and perp_point_tmp.x > ball.center.x) and (cmp_p.y > perp_point_tmp.y and perp_point_tmp.y > ball.center.y)) {
+			bounce_index = i;
+		}
+	}
+	return bounce_index;
 
-	double new_x = ball.center.x - ((ball.center.x - target.x) * power);
-	double new_y = ball.center.y - ((ball.center.y - target.y) * power);
-	Point new_p(new_x, new_y);
-	//pseudo code
-	//if point outside wall 1-4
-	Line ball_line(ball.center, target);
+}
+
+void Field::hit(Point target, double power) {
+	double new_x;
+	double new_y;
+	Point new_p;
+	Line ball_line;
 	Line rectangle[4];
 	rectangle[0] = Line(endPoints[0], endPoints[1]);
 	rectangle[1] = Line(endPoints[1], endPoints[2]);
 	rectangle[2] = Line(endPoints[2], endPoints[3]);
 	rectangle[3] = Line(endPoints[3], endPoints[0]);
-	Point perp_point_tmp;
+	do{
+	double new_x = ball.center.x - ((ball.center.x - target.x) * power);
+	double new_y = ball.center.y - ((ball.center.y - target.y) * power);
+	new_p = Point(new_x, new_y);
+	ball_line = Line(ball.center, target);
+	//pseudo code
+	//if point outside wall 1-4
+	/*Point perp_point_tmp;
 	int bounce_index = -1;
 	for(int i = 0; i < 4; i++){
 		perp_point_tmp = Point((ball_line.B - rectangle[i].B) / (rectangle[i].A - ball_line.A), (ball_line.A * (ball_line.B - rectangle[i].B) / (rectangle[i].A - ball_line.A)) + ball_line.B);
@@ -42,19 +62,35 @@ void Field::hit(Point target, double power) {
 		else if((new_p.x > perp_point_tmp.x and perp_point_tmp.x > ball.center.x) and (new_p.y > perp_point_tmp.y and perp_point_tmp.y > ball.center.y)) {
 			bounce_index = i;
 		}
-	}
+	}*/
+	int bounce_index = collision(ball_line, rectangle, new_p);
 	if(bounce_index != -1){
+		int next_index = bounce_index + 1;
+		if(bounce_index == 3){
+		 next_index = 0;
+		}
+		if(endPoints[bounce_index].y == 0 && endPoints[next_index].y == 0){
+			ball.center.x = new_p.x;
+			ball.center.y = 0 - new_p.y;
+			continue;
+		}
+		else if(endPoints[bounce_index].x == 0 && endPoints[next_index].x == 0){
+			ball.center.y = new_p.y;
+			ball.center.x = 0 - new_p.x;
+			continue;
+		}
 		Line bounce_line_perp(-1 / rectangle[bounce_index].A, new_p.y - (-1 / rectangle[bounce_index].A * new_p.x));
 		double border_point = (rectangle[bounce_index].B - bounce_line_perp.B) / (bounce_line_perp.A - rectangle[bounce_index].A);
 		Point new_point_on_border(border_point, (rectangle[bounce_index].A * border_point) + rectangle[bounce_index].B);
 		Point after_bounce(new_point_on_border.x * 2 - new_p.x, new_point_on_border.y * 2 - new_p.y);
 		ball.center.x = after_bounce.x;
 		ball.center.y = after_bounce.y;
+		continue;
 	}
-	else{
-		ball.center.x = new_p.x;
-		ball.center.y = new_p.y;
-	}
+	ball.center.x = new_p.x;
+	ball.center.y = new_p.y;
+	return;
+	}while(collision(ball_line, rectangle, new_p) != -1);
 		//double border_eq_a1 = (endPoints[0].y - endPoints[1].y)/(endPoints[0].x - endPoints[1].x);
 		//double border_eq_b1 = endPoints[0].y - (border_eq_a1 * endPoints[0].x);
 		//double perpendicular_eq_a = -1 / border_eq_a1;
--- Field.h
+++ Field.h
@@ -10,7 +10,7 @@ public:
 	Field();
 	Field(Point origin, double width, double height, Ball ball);
 	Field(Point endPoints[4], Ball ball);
-
+	int collision(Line ball_l, Line walls[4], Point cmp_p);
 	void hit(Point target, double power);
 
 	friend ostream& operator<<(ostream& os, const Field& f);
